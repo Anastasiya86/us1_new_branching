92b1a006be1822f22cb175676bbcb3d026624da4
anastasiya zlatanova <anastasiya8611@gmail.com>
2022-11-05 09:47:10 -0500

    class user completed

--- src/login/user.java
+++ src/login/user.java
@@ -4,5 +4,6 @@ public class user {
     public static void main(String[] args) {
         System.out.println("start");
         System.out.println("wip");
+        System.out.println("completed");
     }
 }
